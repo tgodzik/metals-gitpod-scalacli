4ab94e9510e3b93390e334ec8e8ba62db4a8e017
Tomasz Godzik <tomek.godzik@gmail.com>
2022-01-24 11:46:30 +0100

    Switch to scalacli

--- gitpod_command.sh
+++ gitpod_command.sh
@@ -6,4 +6,3 @@ APPS_DIR="$METALS_DIR/apps"
 mkdir -p $APPS_DIR
 
 export PATH=$PATH:/usr/local/openjdk-8/bin:$APPS_DIR
-bloop about
--- src/test/scala/SampleTest.scala
+++
@@ -1,9 +0,0 @@
-class SampleTest extends munit.FunSuite {
-  test("zigzag") {
-    assertEquals(1, 1)
-  }
-
-  test("zigzag-fail") {
-    assertEquals(1, 2)
-  }
-}
